2a72825427feb3486ac8f3ddb8780e6a80f60995
Ayush <ayushganguli1769@gmail.com>
2020-03-01 02:34:23 +0530

    added sqlite for convenience

--- water/settings.py
+++ water/settings.py
@@ -78,14 +78,10 @@ WSGI_APPLICATION = 'water.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'robo2',
-        'USER' : 'postgres',
-        'PASSWORD': '1234',
-        'HOST' : 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
